246898550865e4a52a24a6faaf7f11700630f2de
Ignacio Baptista [Notebook] <ibaptista@est.frba.utn.edu.ar>
2020-05-04 12:43:33 -0300

    Funciona tablero generandose ANTES del wait en el while, falta delegar responsabilidades en funcionesW

--- main.py
+++ main.py
@@ -12,17 +12,33 @@ def vecinosVivosdDe(matriz, posicion):
 
     # print("posFila: " + str(posFila) + " posCol: " + str(posCol))
 
-    for unaFilaFuncion in range(posFila - 1, posFila + 2):
-        for unaColFuncion in range(posCol - 1, posCol + 2):
-            # print("Fila: " + str(unaFilaFuncion) + " Columna: " + str(unaColFuncion))
-            if (unaFilaFuncion < 0) | (unaFilaFuncion >= cantFilas) | \
-               (unaColFuncion < 0) | (unaColFuncion >= cantColumnas) | \
-               ((unaFilaFuncion == posFila) & (unaColFuncion == posCol)):
+    for filaFuncion in range(posFila - 1, posFila + 2):
+        for colFuncion in range(posCol - 1, posCol + 2):
+            # print("Fila: " + str(filaFuncion) + " Columna: " + str(colFuncion))
+            if (filaFuncion < 0) | (filaFuncion >= cantFilas) | \
+                    (colFuncion < 0) | (colFuncion >= cantColumnas) | \
+                    ((filaFuncion == posFila) & (colFuncion == posCol)):
                 continue
-            if matriz[unaFilaFuncion][unaColFuncion] == 1:
+            if matriz[filaFuncion][colFuncion] == 1:
                 cantVivas += 1
 
     return cantVivas
+
+
+def printearMatrices():
+    print("Matriz vieja: ")
+
+    for filaFuncion in range(0, cantFilas):
+        for columnaFuncion in range(0, cantColumnas):
+            print(matrizVieja[filaFuncion][columnaFuncion], end="")
+        print("")
+
+    print("Matriz nueva: ")
+
+    for filaFuncion in range(0, cantFilas):
+        for columnaFuncion in range(0, cantColumnas):
+            print(matrizNueva[filaFuncion][columnaFuncion], end="")
+        print("")
 
 
 # Variables Importantes
@@ -40,30 +56,32 @@ anchoVentana = 800
 altoVentana = 800
 segundosDelay = 1
 
+# Setup Inicial
 ventana = pygame.display.set_mode((anchoVentana, altoVentana))
 
 pygame.display.set_caption("Primer Juego")
 
 bloquesGrises = [(2, 2), (2, 4), (25, 29), (0, 0), (0, 1), (0, 2), (1, 1),
-                 (1, 0), (15, 16), (15, 17), (15, 18), (15, 19), (16, 16), (16, 17), (16, 18), (16, 19), (17, 16), (17, 17), (17, 18), (17, 19), (17, 20), (17, 21), (17, 22), (17, 23), (17, 24), (17, 25), (17, 26)]
+                 (1, 0), (15, 16), (15, 17), (15, 18), (15, 19), (16, 16), (16, 17), (16, 18), (16, 19), (17, 16),
+                 (17, 17), (17, 18), (17, 19), (17, 20), (17, 21), (17, 22), (17, 23), (17, 24), (17, 25), (17, 26)]
 
-matrizVieja = [[0 for a in range(cantFilas)] for b in range(cantColumnas)]
-matrizNueva = [[0 for a in range(cantFilas)] for b in range(cantColumnas)]
+matrizVieja = [[0 for vx in range(cantFilas)] for vy in range(cantColumnas)]
+matrizNueva = [[0 for nx in range(cantFilas)] for ny in range(cantColumnas)]
 
 run = True
 
 primeraVuelta = True
 
+# Bucle infinito
 while run:
-    pygame.time.delay(segundosDelay * 100)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     # Generar matriz de cuadrados
-    for laFila in range(0, cantColumnas):
-        for laColumna in range(0, cantFilas):
+    for laFila in range(0, cantFilas):
+        for laColumna in range(0, cantColumnas):
 
             colores = (100, 100, 100)
 
@@ -87,12 +105,13 @@ while run:
 
     primeraVuelta = False
 
-    for laFila in range(0, cantColumnas):
-        for laColumna in range(0, cantFilas):
+    # Generar matrizNueva (basandose en datos de matrizVieja)
+    for laFila in range(0, cantFilas):
+        for laColumna in range(0, cantColumnas):
             # La celda está viva
             if matrizVieja[laFila][laColumna] == 1:
                 if (vecinosVivosdDe(matrizVieja, (laFila, laColumna)) == 2) | \
-                   (vecinosVivosdDe(matrizVieja, (laFila, laColumna)) == 3):
+                        (vecinosVivosdDe(matrizVieja, (laFila, laColumna)) == 3):
                     matrizNueva[laFila][laColumna] = 1
                 else:
                     matrizNueva[laFila][laColumna] = 0
@@ -105,25 +124,9 @@ while run:
 
     # print("Vecinos vivos de 15,16: " + str(vecinosVivosdDe(matrizVieja, (16, 17))))
 
-    '''
-    print("Matriz vieja: ")
-
-    for laFila in range(0, cantColumnas):
-        for laColumna in range(0, cantFilas):
-            print(matrizVieja[laFila][laColumna], end="")
-        print("")
-
-    print("Matriz nueva: ")
-
-    for laFila in range(0, cantColumnas):
-        for laColumna in range(0, cantFilas):
-            print(matrizNueva[laFila][laColumna], end="")
-        print("")
-    '''
-
-    # Pasar todos los datos de la matriz nueva a la vieja para repetir ciclo
-    for laFila in range(0, cantColumnas):
-        for laColumna in range(0, cantFilas):
+    # Pasar todos los datos de matrizNueva a matrizVieja para repetir ciclo
+    for laFila in range(0, cantFilas):
+        for laColumna in range(0, cantColumnas):
             matrizVieja[laFila][laColumna] = matrizNueva[laFila][laColumna]
 
     # mouse = pygame.mouse.get_pos()
@@ -131,5 +134,7 @@ while run:
 
     # Actualizar display
     pygame.display.update()
+
+    pygame.time.delay(segundosDelay * 1000)
 
 pygame.quit()
