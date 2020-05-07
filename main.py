0b583b1ace1beca23cc0e5fdb4c2a5a3e2bb0fa3
Ignacio Baptista [Notebook] <ibaptista@est.frba.utn.edu.ar>
2020-05-06 21:27:41 -0300

    Resuelto bug :D funcionando tocar bloques tanto con como sin pausa

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import pygame
+from datetime import datetime
 
 pygame.init()
 
 
 # Funciones
-def printearMatrices():
-    print("Matriz vieja: ")
 
-    for filaFuncion in range(0, cantFilas):
-        for columnaFuncion in range(0, cantColumnas):
-            print(matrizVieja[filaFuncion][columnaFuncion], end="")
-        print("")
+def hacerUnLog(unString):  # Incompleto
+    # Dia y hora actuales
+    now = datetime.now()
 
+    timestamp = datetime.timestamp(now)
+    print("timestamp =", timestamp)
+
+
+def printearMatrizNueva():
     print("Matriz nueva: ")
-
     for filaFuncion in range(0, cantFilas):
         for columnaFuncion in range(0, cantColumnas):
             print(matrizNueva[filaFuncion][columnaFuncion], end="")
+        print("")
+
+
+def printearMatrizVieja():
+    print("Matriz vieja: ")
+    for filaFuncion in range(0, cantFilas):
+        for columnaFuncion in range(0, cantColumnas):
+            print(matrizVieja[filaFuncion][columnaFuncion], end="")
         print("")
 
 
@@ -26,11 +36,8 @@ def vecinosVivosdDe(matriz, posicion):
 
     cantVivas = 0
 
-    # print("posFila: " + str(posFila) + " posCol: " + str(posCol))
-
     for filaFuncion in range(posFila - 1, posFila + 2):
         for colFuncion in range(posCol - 1, posCol + 2):
-            # print("Fila: " + str(filaFuncion) + " Columna: " + str(colFuncion))
             if (filaFuncion < 0) | (filaFuncion >= cantFilas) | \
                     (colFuncion < 0) | (colFuncion >= cantColumnas) | \
                     ((filaFuncion == posFila) & (colFuncion == posCol)):
@@ -41,22 +48,26 @@ def vecinosVivosdDe(matriz, posicion):
     return cantVivas
 
 
-def generarMatrizVieja():
-    print("generarMatrizVieja")
+def mostrarMatrizNueva():
+    print("mostrarMatrizNueva")
     # Generar matriz de cuadrados
     for filaFuncion in range(0, cantFilas):
         for columnaFuncion in range(0, cantColumnas):
 
-            colores = (100, 100, 100)
+            colores = colorMuerto
 
-            if matrizVieja[filaFuncion][columnaFuncion] == 1:
-                colores = (255, 255, 255)
+            if matrizNueva[filaFuncion][columnaFuncion] == 1:
+                colores = colorVivo
 
-            # random.randrange(256)
-            pygame.draw.rect(ventana,
-                             colores,  # Color
-                             (x + columnaFuncion * (ancho + 3), y + filaFuncion * (alto + 3), ancho,
-                              alto))  # posx, posy, ancho, alto
+            dibujarRectangulo(colores, columnaFuncion, filaFuncion)
+    printearMatrizNueva()
+
+
+def dibujarRectangulo(colores, columna, fila):
+    pygame.draw.rect(ventana,
+                     colores,  # Color
+                     (x + columna * (ancho + 3), y + fila * (alto + 3), ancho,
+                      alto))  # posx, posy, ancho, alto
 
 
 def generarMatrizNueva():
@@ -79,26 +90,24 @@ def generarMatrizNueva():
                     matrizNueva[laFila][laColumna] = 0
 
 
-def generarMatrizPrimeraVez():
-    print("generarMatrizPrimeraVez")
+def generarMatrizVieja():
+    print("generarMatrizVieja")
     # Generar matriz de cuadrados
     for filaFuncion in range(0, cantFilas):
         for columnaFuncion in range(0, cantColumnas):
 
-            colores = (100, 100, 100)
+            colores = colorMuerto
 
+            # Evaluar las posiciones de los bloques iniciales
             for (unaFila, unaCol) in bloquesGrises:
                 if (filaFuncion == unaFila) & (columnaFuncion == unaCol):
-                    colores = (255, 255, 255)
+                    colores = colorVivo
                     matrizVieja[filaFuncion][columnaFuncion] = 1
                     break
                 else:
                     matrizVieja[filaFuncion][columnaFuncion] = 0
 
-            pygame.draw.rect(ventana,
-                             colores,  # Color
-                             (x + columnaFuncion * (ancho + 3), y + filaFuncion * (alto + 3), ancho,
-                              alto))  # posx, posy, ancho, alto
+            dibujarRectangulo(colores, columnaFuncion, filaFuncion)
 
 
 def pasarMatrizNuevaAVieja():
@@ -110,7 +119,6 @@ def pasarMatrizNuevaAVieja():
 
 
 def manejarPausa(estaPausado):
-
     print("manejarPausa")
     radioCirculo = 20
     anchoCirculo = 10
@@ -153,9 +161,12 @@ def modificarCelda(unaPos):
                 print("Tocaste un cuadrado: Fila: " + str(laFila) + " Columna:" + str(laColumna))
                 if matrizVieja[laFila][laColumna] == 1:
                     matrizVieja[laFila][laColumna] = 0
+                    print("Dibujando cuadrado.. de color muerto")
+                    dibujarRectangulo(colorMuerto, laColumna, laFila)
                 else:
+                    print("Dibujando cuadrado.. de color vivo")
                     matrizVieja[laFila][laColumna] = 1
-                generarMatrizVieja()
+                    dibujarRectangulo(colorVivo, laColumna, laFila)
                 return
     print("No tocaste ningun cuadrado")
 
@@ -168,7 +179,7 @@ y = 20
 ancho = 10
 alto = 10
 # Matriz
-cantFilas = 60
+cantFilas = 60  # Default 60
 cantColumnas = 60
 # Configuracion
 anchoVentana = 900
@@ -176,6 +187,8 @@ altoVentana = 900
 segundosDelay = 1
 run = True
 estaEnPausa = True
+colorMuerto = (100, 100, 100)
+colorVivo = (255, 255, 255)
 
 pygame.display.set_caption("El juego de la vida de Conway")
 
@@ -183,13 +196,15 @@ bloquesGrises = [(2, 2), (2, 4), (25, 29), (25, 28), (25, 27), (25, 26), (25, 25
                  (1, 0), (15, 16), (15, 17), (15, 18), (15, 19), (16, 16), (16, 17), (16, 18), (16, 19), (17, 16),
                  (17, 17), (17, 18), (17, 19), (17, 20), (17, 21), (17, 22), (17, 23), (17, 24), (17, 25), (17, 26)]
 
+# bloquesGrises = [(0, 0)]
+
 matrizVieja = [[0 for vx in range(cantFilas)] for vy in range(cantColumnas)]
 matrizNueva = [[0 for nx in range(cantFilas)] for ny in range(cantColumnas)]
 
 # Setup Inicial
 ventana = pygame.display.set_mode((anchoVentana, altoVentana))
 
-generarMatrizPrimeraVez()
+generarMatrizVieja()
 pygame.display.update()
 
 # Bucle infinito
@@ -204,18 +219,16 @@ while run:
             modificarCelda(pygame.mouse.get_pos())
             continue
 
-    printearMatrices()
-
-    generarMatrizVieja()
-
     generarMatrizNueva()
 
     pasarMatrizNuevaAVieja()
 
+    mostrarMatrizNueva()
+
     # Actualizar display
     pygame.display.update()
 
-    pygame.time.delay(segundosDelay * 1000)
+    pygame.time.delay(segundosDelay * 200)
 
 pygame.quit()
 
